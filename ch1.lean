43e77f9f48d0ac359728eb0aafde58e4fdde9e63
Gregory Wickham <gwickham99@gmail.com>
2026-05-09 10:39:09 -0700

    finished example 0.4

--- ch1.lean
+++ ch1.lean
@@ -22,14 +22,13 @@ structure killingTerm X where
 @[ext]
 structure finiteGraphOver X where
   finiteVertices : Fintype X
-  vertices : Finset X
-  allVertices : ∀ (x : X), x ∈ vertices
+  vertices := finiteVertices.elems
   b : edgeWeight X
   c : killingTerm X
 
 namespace finiteGraphOver
 
-variable {X : Type*} (g : finiteGraphOver X)
+variable {X : Type*} (g : finiteGraphOver X) (x : X)
 
 def isEdge (e : X × X) := 0 < g.b.toFun e.1 e.2
 
@@ -39,9 +38,12 @@ lemma edge_def (e : X × X) : e ∈ g.edgeSet ↔ g.isEdge e := by rfl
 
 lemma edgeSet_def (e : X × X) : e ∈ g.edgeSet ↔ 0 < g.b.toFun e.1 e.2 := by rfl
 
-lemma isEdge_def (e : X × X) : e ∈ g.edgeSet ↔ 0 < g.b.toFun e.1 e.2 := by rfl
+lemma isEdge_def (e : X × X) : g.isEdge e ↔ 0 < g.b.toFun e.1 e.2 := by rfl
 
 def neighbors (x y : X) := 0 < g.b.toFun x y
+
+noncomputable
+instance : DecidablePred (g.neighbors x) := instDecidablePredComp
 
 structure StandardWeights : Prop where
   b_binary : ∀ x y : X, g.b.toFun x y ∈ {n | n = 0 ∨ n = 1}
@@ -49,7 +51,7 @@ structure StandardWeights : Prop where
   c_zero : ∀ x : X, g.c.toFun x = 0
 
 -- Example 0.2 (Graphs with standard weights)
-example (g : finiteGraphOver X) (h : StandardWeights g) :
+lemma standardWeightEdgeSet (h : StandardWeights g) :
     g.edgeSet = {(x, y) : X × X | g.b.toFun x y = 1} := by
   ext e
   simp only [edgeSet_def, Set.mem_setOf_eq]
@@ -59,5 +61,46 @@ example (g : finiteGraphOver X) (h : StandardWeights g) :
   | inr h_2 => simp_all only [zero_lt_one]
 
 -- Definition 0.3 (Degree)
-def degree (g : finiteGraphOver X) (x : X) : ℝ≥0 :=
-  ∑ y : g.vertices, (g.b.toFun x y) + (g.c.toFun x)
+def degree (x : X) : ℝ≥0 :=
+  ∑ y ∈ g.vertices, (g.b.toFun x y) + (g.c.toFun x)
+
+-- Example 0.4 (Combinatorial Degree)
+lemma degreeWithStandardWeights (h : StandardWeights g) (x : X) :
+    g.degree x = ∑ y ∈ g.vertices with g.neighbors x y, 1 := by
+  simp only [degree, h.c_zero, add_zero, neighbors]
+  have hyp := h.b_binary' x
+  have gt0isEq1 (y : X) : 0 < g.b.toFun x y ↔ g.b.toFun x y = 1 := by
+    constructor
+    · have := hyp y
+      intro a
+      cases this with
+      | inl h1 => simp_all only [lt_self_iff_false]
+      | inr h2 => simp_all only [zero_lt_one]
+    · intro a
+      simp_all only [zero_lt_one]
+  have setGt0Eq1 : {y ∈ g.vertices | 0 < g.b.toFun x y} = {y ∈ g.vertices | g.b.toFun x y = 1} := by
+    simp_all only
+  have ignore0 := Finset.sum_filter_ne_zero g.vertices (f := (fun y ↦ g.b.toFun x y))
+  rw [setGt0Eq1]
+  rw [← ignore0]
+  have (y : X) : g.b.toFun x y ≠ 0 ↔ g.b.toFun x y = 1 := by
+    constructor
+    · have := hyp y
+      intro a
+      cases this with
+      | inl h1 => simp_all only [ne_eq, not_true_eq_false]
+      | inr h2 => exact h2
+    · exact fun a ↦ ne_zero_of_eq_one a
+  have : ∑ x_1 ∈ g.vertices with g.b.toFun x x_1 ≠ 0, g.b.toFun x x_1 =
+    ∑ x_1 ∈ g.vertices with g.b.toFun x x_1 = 1, g.b.toFun x x_1 := by
+    simp_all only [ne_eq]
+  rw [this]
+  apply Finset.sum_congr -- try re-working using this from beginning
+  · rfl
+  simp
+
+lemma degreeWithStandardWeightsCard (h : StandardWeights g) (x : X) :
+    g.degree x = {y ∈ g.vertices | g.neighbors x y}.card := by
+  simp [degreeWithStandardWeights g h x]
+
+end finiteGraphOver
