7f4b37f1e389c89905715865dcb3d9ed678f5934
Gregory Wickham <gwickham99@gmail.com>
2026-05-09 11:21:33 -0700

    working on proving bilinear form

--- ch1.lean
+++ ch1.lean
@@ -3,6 +3,9 @@ import Mathlib.Combinatorics.Graph.Basic
 import Mathlib.Data.Real.Basic
 import Mathlib.Data.NNReal.Defs
 import Mathlib.Algebra.BigOperators.Group.Finset.Defs
+import Mathlib.Topology.ContinuousMap.Defs
+import Mathlib.Topology.Order
+import Mathlib.LinearAlgebra.BilinearMap
 
 #check LinearMap.BilinForm.toMatrix' (R₁ := ℝ)
 
@@ -28,31 +31,31 @@ structure finiteGraphOver X where
 
 namespace finiteGraphOver
 
-variable {X : Type*} (g : finiteGraphOver X) (x : X)
+variable {X : Type*} (G : finiteGraphOver X) (x : X)
 
-def isEdge (e : X × X) := 0 < g.b.toFun e.1 e.2
+def isEdge (e : X × X) := 0 < G.b.toFun e.1 e.2
 
-def edgeSet := {(x, y) : X × X | 0 < g.b.toFun x y}
+def edgeSet := {(x, y) : X × X | 0 < G.b.toFun x y}
 
-lemma edge_def (e : X × X) : e ∈ g.edgeSet ↔ g.isEdge e := by rfl
+lemma edge_def (e : X × X) : e ∈ G.edgeSet ↔ G.isEdge e := by rfl
 
-lemma edgeSet_def (e : X × X) : e ∈ g.edgeSet ↔ 0 < g.b.toFun e.1 e.2 := by rfl
+lemma edgeSet_def (e : X × X) : e ∈ G.edgeSet ↔ 0 < G.b.toFun e.1 e.2 := by rfl
 
-lemma isEdge_def (e : X × X) : g.isEdge e ↔ 0 < g.b.toFun e.1 e.2 := by rfl
+lemma isEdge_def (e : X × X) : G.isEdge e ↔ 0 < G.b.toFun e.1 e.2 := by rfl
 
-def neighbors (x y : X) := 0 < g.b.toFun x y
+def neighbors (x y : X) := 0 < G.b.toFun x y
 
 noncomputable
-instance : DecidablePred (g.neighbors x) := instDecidablePredComp
+instance : DecidablePred (G.neighbors x) := instDecidablePredComp
 
 structure StandardWeights : Prop where
-  b_binary : ∀ x y : X, g.b.toFun x y ∈ {n | n = 0 ∨ n = 1}
-  b_binary' : ∀ x y : X, g.b.toFun x y = 0 ∨ g.b.toFun x y = 1
-  c_zero : ∀ x : X, g.c.toFun x = 0
+  b_binary : ∀ x y : X, G.b.toFun x y ∈ {n | n = 0 ∨ n = 1}
+  b_binary' : ∀ x y : X, G.b.toFun x y = 0 ∨ G.b.toFun x y = 1
+  c_zero : ∀ x : X, G.c.toFun x = 0
 
 -- Example 0.2 (Graphs with standard weights)
-lemma standardWeightEdgeSet (h : StandardWeights g) :
-    g.edgeSet = {(x, y) : X × X | g.b.toFun x y = 1} := by
+lemma standardWeightEdgeSet (h : StandardWeights G) :
+    G.edgeSet = {(x, y) : X × X | G.b.toFun x y = 1} := by
   ext e
   simp only [edgeSet_def, Set.mem_setOf_eq]
   have hyp := h.b_binary' e.1 e.2
@@ -62,14 +65,14 @@ lemma standardWeightEdgeSet (h : StandardWeights g) :
 
 -- Definition 0.3 (Degree)
 def degree (x : X) : ℝ≥0 :=
-  ∑ y ∈ g.vertices, (g.b.toFun x y) + (g.c.toFun x)
+  ∑ y ∈ G.vertices, (G.b.toFun x y) + (G.c.toFun x)
 
 -- Example 0.4 (Combinatorial Degree)
-lemma degreeWithStandardWeights (h : StandardWeights g) (x : X) :
-    g.degree x = ∑ y ∈ g.vertices with g.neighbors x y, 1 := by
+lemma degreeWithStandardWeights (h : StandardWeights G) (x : X) :
+    G.degree x = ∑ y ∈ G.vertices with G.neighbors x y, 1 := by
   simp only [degree, h.c_zero, add_zero, neighbors]
   have hyp := h.b_binary' x
-  have gt0isEq1 (y : X) : 0 < g.b.toFun x y ↔ g.b.toFun x y = 1 := by
+  have gt0isEq1 (y : X) : 0 < G.b.toFun x y ↔ G.b.toFun x y = 1 := by
     constructor
     · have := hyp y
       intro a
@@ -78,12 +81,11 @@ lemma degreeWithStandardWeights (h : StandardWeights g) (x : X) :
       | inr h2 => simp_all only [zero_lt_one]
     · intro a
       simp_all only [zero_lt_one]
-  have setGt0Eq1 : {y ∈ g.vertices | 0 < g.b.toFun x y} = {y ∈ g.vertices | g.b.toFun x y = 1} := by
+  have setGt0Eq1 : {y ∈ G.vertices | 0 < G.b.toFun x y} = {y ∈ G.vertices | G.b.toFun x y = 1} := by
     simp_all only
-  have ignore0 := Finset.sum_filter_ne_zero g.vertices (f := (fun y ↦ g.b.toFun x y))
-  rw [setGt0Eq1]
-  rw [← ignore0]
-  have (y : X) : g.b.toFun x y ≠ 0 ↔ g.b.toFun x y = 1 := by
+  have ignore0 := Finset.sum_filter_ne_zero G.vertices (f := (fun y ↦ G.b.toFun x y))
+  rw [setGt0Eq1, ← ignore0]
+  have (y : X) : G.b.toFun x y ≠ 0 ↔ G.b.toFun x y = 1 := by
     constructor
     · have := hyp y
       intro a
@@ -91,16 +93,41 @@ lemma degreeWithStandardWeights (h : StandardWeights g) (x : X) :
       | inl h1 => simp_all only [ne_eq, not_true_eq_false]
       | inr h2 => exact h2
     · exact fun a ↦ ne_zero_of_eq_one a
-  have : ∑ x_1 ∈ g.vertices with g.b.toFun x x_1 ≠ 0, g.b.toFun x x_1 =
-    ∑ x_1 ∈ g.vertices with g.b.toFun x x_1 = 1, g.b.toFun x x_1 := by
+  have : ∑ x_1 ∈ G.vertices with G.b.toFun x x_1 ≠ 0, G.b.toFun x x_1 =
+    ∑ x_1 ∈ G.vertices with G.b.toFun x x_1 = 1, G.b.toFun x x_1 := by
     simp_all only [ne_eq]
   rw [this]
   apply Finset.sum_congr -- try re-working using this from beginning
   · rfl
   simp
 
-lemma degreeWithStandardWeightsCard (h : StandardWeights g) (x : X) :
-    g.degree x = {y ∈ g.vertices | g.neighbors x y}.card := by
-  simp [degreeWithStandardWeights g h x]
+lemma degreeWithStandardWeightsCard (h : StandardWeights G) (x : X) :
+    G.degree x = {y ∈ G.vertices | G.neighbors x y}.card := by
+  simp [degreeWithStandardWeights G h x]
+
+-- Section 0.1.2
+--variable [TopologicalSpace X] [DiscreteTopology X]
+--variable {F : Type*} [ContinuousMapClass F X ℝ] (f : F)
+-- Don't deal with topologies, just consider all functions
+
+noncomputable
+def Qfun (f g : X → ℝ) :=
+  (1/2) * ∑ x ∈ G.vertices, ∑ y ∈ G.vertices, (G.b.toFun x y) * (f x - f y) * (g x - g y) +
+    ∑ x ∈ G.vertices, (G.c.toFun x) * f x * g x
+
+instance Q' : IsBilinearMap G.Qfun (R := ℝ) where
+  add_left f g y := by
+    dsimp [Qfun]
+    sorry
+  smul_left := sorry
+  add_right := sorry
+  smul_right := sorry
+
+noncomputable
+def Q := G.Q'.toLinearMap
+
+#check G.Q
+
+
 
 end finiteGraphOver
