6fc8bfe14ea94c8bd93cbd78e4e23644711da24f
Gregory Wickham <gwickham99@gmail.com>
2026-05-10 06:58:42 -0700

    some fixes

--- ch1.lean
+++ ch1.lean
@@ -9,7 +9,7 @@ import Mathlib.LinearAlgebra.BilinearMap
 import Mathlib.Combinatorics.SimpleGraph.LapMatrix
 
 open NNReal
-
+/-
 @[ext]
 structure edgeWeight X where
   toFun : X → X → ℝ≥0
@@ -19,42 +19,47 @@ structure edgeWeight X where
 @[ext]
 structure killingTerm X where
   toFun : X → ℝ≥0
-
+-/
 -- Definition 0.1 (Graph over finite X)
 @[ext]
 structure finiteGraphOver X where
   finiteVertices : Fintype X
-  vertices := finiteVertices.elems
-  b : edgeWeight X
-  c : killingTerm X
+  b : X → X → ℝ≥0
+  symm : ∀ ⦃x y : X⦄, b x y = b y x
+  no_loop : ∀ ⦃x : X⦄, b x x = 0
+  c : X → ℝ≥0
 
+
+set_option linter.unusedFintypeInType false
 namespace finiteGraphOver
 
 variable {X : Type*} (G : finiteGraphOver X) (x : X)
+--instance : Fintype X := G.finiteVertices
+variable [Fintype X] -- I should be able to eliminate this though, shouldn't I?
 
-def isEdge (e : X × X) := 0 < G.b.toFun e.1 e.2
+def isEdge (e : X × X) := 0 < G.b e.1 e.2
 
-def edgeSet := {(x, y) : X × X | 0 < G.b.toFun x y}
+def edgeSet := {(x, y) : X × X | 0 < G.b x y}
 
 lemma edge_def (e : X × X) : e ∈ G.edgeSet ↔ G.isEdge e := by rfl
 
-lemma edgeSet_def (e : X × X) : e ∈ G.edgeSet ↔ 0 < G.b.toFun e.1 e.2 := by rfl
+lemma edgeSet_def (e : X × X) : e ∈ G.edgeSet ↔ 0 < G.b e.1 e.2 := by rfl
 
-lemma isEdge_def (e : X × X) : G.isEdge e ↔ 0 < G.b.toFun e.1 e.2 := by rfl
+lemma isEdge_def (e : X × X) : G.isEdge e ↔ 0 < G.b e.1 e.2 := by rfl
 
-def neighbors (x y : X) := 0 < G.b.toFun x y
+def neighbors (x y : X) := 0 < G.b x y
 
 noncomputable
 instance : DecidablePred (G.neighbors x) := instDecidablePredComp
 
 structure StandardWeights : Prop where
-  b_binary : ∀ x y : X, G.b.toFun x y ∈ {n | n = 0 ∨ n = 1}
-  b_binary' : ∀ x y : X, G.b.toFun x y = 0 ∨ G.b.toFun x y = 1
-  c_zero : ∀ x : X, G.c.toFun x = 0
+  b_binary : ∀ x y : X, G.b x y ∈ {n | n = 0 ∨ n = 1}
+  b_binary' : ∀ x y : X, G.b x y = 0 ∨ G.b x y = 1
+  c_zero : ∀ x : X, G.c x = 0
 
 -- Example 0.2 (Graphs with standard weights)
 lemma standardWeightEdgeSet (h : StandardWeights G) :
-    G.edgeSet = {(x, y) : X × X | G.b.toFun x y = 1} := by
+    G.edgeSet = {(x, y) : X × X | G.b x y = 1} := by
   ext e
   simp only [edgeSet_def, Set.mem_setOf_eq]
   have hyp := h.b_binary' e.1 e.2
@@ -64,27 +69,25 @@ lemma standardWeightEdgeSet (h : StandardWeights G) :
 
 -- Definition 0.3 (Degree)
 def degree (x : X) : ℝ≥0 :=
-  ∑ y ∈ G.vertices, (G.b.toFun x y) + (G.c.toFun x)
+  ∑ y, (G.b x y) + (G.c x)
 
 -- Example 0.4 (Combinatorial Degree)
 lemma degreeWithStandardWeights (h : StandardWeights G) (x : X) :
-    G.degree x = ∑ y ∈ G.vertices with G.neighbors x y, 1 := by
+    G.degree x = ∑ y with G.neighbors x y, 1 := by
   simp only [degree, h.c_zero, add_zero, neighbors]
   have hyp := h.b_binary' x
-  have gt0isEq1 (y : X) : 0 < G.b.toFun x y ↔ G.b.toFun x y = 1 := by
+  have gt0isEq1 (y : X) : 0 < G.b x y ↔ G.b x y = 1 := by
     constructor
     · have := hyp y
-      intro a
+      intro
       cases this with
       | inl h1 => simp_all only [lt_self_iff_false]
       | inr h2 => simp_all only [zero_lt_one]
-    · intro a
+    · intro
       simp_all only [zero_lt_one]
-  have setGt0Eq1 : {y ∈ G.vertices | 0 < G.b.toFun x y} = {y ∈ G.vertices | G.b.toFun x y = 1} := by
-    simp_all only
-  have ignore0 := Finset.sum_filter_ne_zero G.vertices (f := (fun y ↦ G.b.toFun x y))
-  rw [setGt0Eq1, ← ignore0]
-  have (y : X) : G.b.toFun x y ≠ 0 ↔ G.b.toFun x y = 1 := by
+  have sumEqSumOfNon0 : ∑ y, G.b x y = ∑ y with G.b x y ≠ 0, G.b x y :=
+    Eq.symm (Finset.sum_filter_ne_zero Finset.univ)
+  have neq0iff1 (y : X) : G.b x y ≠ 0 ↔ G.b x y = 1 := by
     constructor
     · have := hyp y
       intro a
@@ -92,25 +95,19 @@ lemma degreeWithStandardWeights (h : StandardWeights G) (x : X) :
       | inl h1 => simp_all only [ne_eq, not_true_eq_false]
       | inr h2 => exact h2
     · exact fun a ↦ ne_zero_of_eq_one a
-  have : ∑ x_1 ∈ G.vertices with G.b.toFun x x_1 ≠ 0, G.b.toFun x x_1 =
-    ∑ x_1 ∈ G.vertices with G.b.toFun x x_1 = 1, G.b.toFun x x_1 := by
-    simp_all only [ne_eq]
-  rw [this]
-  apply Finset.sum_congr -- try re-working using this from beginning
+  simp only [sumEqSumOfNon0, gt0isEq1, neq0iff1]
+  apply Finset.sum_congr -- alternatively use congr!; simp_all
   · rfl
   simp
 
 lemma degreeWithStandardWeightsCard (h : StandardWeights G) (x : X) :
-    G.degree x = {y ∈ G.vertices | G.neighbors x y}.card := by
+    G.degree x = Finset.card {y | G.neighbors x y} := by
   simp [degreeWithStandardWeights G h x]
 
 -- Section 0.1.2
 --variable [TopologicalSpace X] [DiscreteTopology X]
 --variable {F : Type*} [ContinuousMapClass F X ℝ] (f : F)
 -- Don't deal with topologies, just consider all functions
-
---instance : Fintype X := G.finiteVertices
-variable [Fintype X] -- I should be able to eliminate this though, shouldn't I?
 
 noncomputable
 instance : DecidableEq X := Classical.typeDecidableEq X
@@ -127,12 +124,14 @@ noncomputable
 def opInducedByMatrix (M : Matrix X X ℝ) :=
   (LinearMap.toMatrix stdBasis stdBasis).symm M
 
+scoped notation "𝟙_" y:max => Pi.single y 1
+
 lemma opMatrixEntryVal (L : (X → ℝ) →ₗ[ℝ] (X → ℝ)) (x y : X) :
-    (matrixAssociatedToOp L) x y = L (Pi.single y 1) x := by
+    (matrixAssociatedToOp L) x y = L (𝟙_y) x := by
   simp [matrixAssociatedToOp, stdBasis, LinearMap.toMatrix_eq_toMatrix', LinearMap.toMatrix'_apply]
 
 lemma opDef (L : (X → ℝ) →ₗ[ℝ] (X → ℝ)) (f : X → ℝ) :
-  (L f) x = ∑ (y : X), (matrixAssociatedToOp L) x y * f y := by
+  (L f) x = ∑ y, (matrixAssociatedToOp L) x y * f y := by
   unfold matrixAssociatedToOp
   have : f = stdBasis.equivFun.symm f := by rfl
   nth_rw 1 [this, Module.Basis.equivFun_symm_apply stdBasis f]
@@ -149,20 +148,20 @@ def IsSymmOp (L : (X → ℝ) →ₗ[ℝ] (X → ℝ)) := (matrixAssociatedToOp 
 -- Forms
 
 noncomputable
-def Qfun (f g : X → ℝ) :=
-  (1/2) * ∑ x : X, ∑ y : X, (G.b.toFun x y) * (f x - f y) * (g x - g y) +
-    ∑ x : X, (G.c.toFun x) * f x * g x
+def associatedFormFun (f g : X → ℝ) :=
+  (1/2) * ∑ x, ∑ y, (G.b x y) * (f x - f y) * (g x - g y) +
+    ∑ x, (G.c x) * f x * g x
 
-instance Q' : IsBilinearMap (R := ℝ) G.Qfun where
+instance associatedFormBilinearMap : IsBilinearMap (R := ℝ) G.associatedFormFun where
   add_left f g h := by
-    unfold Qfun
+    unfold associatedFormFun
     apply sub_eq_zero.mp
     simp [sub_add_eq_sub_sub]
     ring_nf
     simp [Finset.sum_add_distrib]
     ring
   smul_left c f g := by
-    unfold Qfun
+    unfold associatedFormFun
     field_simp
     ring_nf
     simp [Finset.sum_add_distrib]
@@ -170,14 +169,14 @@ instance Q' : IsBilinearMap (R := ℝ) G.Qfun where
     simp [Finset.mul_sum]
     ring_nf
   add_right f g h := by
-    unfold Qfun
+    unfold associatedFormFun
     apply sub_eq_zero.mp
     simp [sub_add_eq_sub_sub]
     ring_nf
     simp [Finset.sum_add_distrib]
     ring
   smul_right c f g := by
-    unfold Qfun
+    unfold associatedFormFun
     ring_nf
     simp [Finset.sum_add_distrib]
     ring_nf
@@ -186,13 +185,12 @@ instance Q' : IsBilinearMap (R := ℝ) G.Qfun where
     apply sub_eq_zero.mp
     ring_nf
 
--- rename to form associated to graph G
 noncomputable
-def Q := G.Q'.toLinearMap
+def associatedForm := G.associatedFormBilinearMap.toLinearMap
 
 open LinearMap
 
-lemma Q_apply {f g} : G.Q f g = G.Qfun f g := by rfl
+lemma associatedForm_apply {f g} : G.associatedForm f g = G.associatedFormFun f g := by rfl
 
 noncomputable
 def matrixAssociatedToForm (Q : LinearMap.BilinForm ℝ (X → ℝ)) :=
@@ -200,7 +198,7 @@ def matrixAssociatedToForm (Q : LinearMap.BilinForm ℝ (X → ℝ)) :=
 
 -- def formInducedByMatrix (M : Matrix X X ℝ) -- may not need to define this
 
-#check matrixAssociatedToForm G.Q
+#check matrixAssociatedToForm G.associatedForm
 
 -- resume at p. 10
 
