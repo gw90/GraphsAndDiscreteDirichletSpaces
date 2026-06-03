283139c904a4154141b643fa22b9a9ecdf5b99a3
Gregory Wickham <gwickham99@gmail.com>
2026-06-03 13:24:11 -0400

    some notation improvements

--- GraphsAndDiscreteDirichletSpaces/Section011'.lean
+++ GraphsAndDiscreteDirichletSpaces/Section011'.lean
@@ -12,39 +12,44 @@ structure GraphOver (X : Type*) extends SimpleGraph X where
 set_option linter.unusedFintypeInType false
 namespace GraphOver
 
+abbrev b {X : Type*} (G : GraphOver X) := G.edgeWeight
+abbrev c {X : Type*} (G : GraphOver X) := G.killingTerm
+
 variable {X : Type*} (G : GraphOver X) (x y : X)
-variable [Fintype X] -- I should be able to eliminate this though, shouldn't I?
 
 structure StandardWeights (G : GraphOver X) : Prop where
-  edgeWeight_binary : ∀ x y : X, G.edgeWeight x y ∈ {n | n = 0 ∨ n = 1}
-  edgeWeight_binary' : ∀ x y : X, G.edgeWeight x y = 0 ∨ G.edgeWeight x y = 1
-  killingTerm_zero : ∀ x : X, G.killingTerm x = 0
+  edgeWeight_binary : ∀ x y : X, G.b x y = 0 ∨ G.b x y = 1
+  killingTerm_zero : ∀ x : X, G.c x = 0
+
+-- To-do: define a coercion for when we have Standard Weights
 
 -- Example 0.2 (Graphs with standard weights)
 lemma standardWeightEdgeSet (h : StandardWeights G) :
-    s(v, w) ∈ G.edgeSet ↔ G.edgeWeight v w = 1 := by
+    s(v, w) ∈ G.edgeSet ↔ G.b v w = 1 := by
   constructor
   · intro h'
-    have hyp := h.edgeWeight_binary' v w
+    have hyp := h.edgeWeight_binary v w
     cases hyp with
     | inl h_1 => simp_all [G.edgeDef v w]
     | inr h_2 => exact h_2
   intro h'
-  have hyp := h.edgeWeight_binary' v w
+  have hyp := h.edgeWeight_binary v w
   cases hyp with
   | inl h_1 => simp_all
   | inr h_2 => simp_all [G.edgeDef v w]
 
+variable [Fintype X] -- I should be able to eliminate this though, shouldn't I?
 -- Definition 0.3 (Degree)
 def deg (x : X) : ℝ≥0 :=
-  ∑ y, (G.edgeWeight x y) + (G.killingTerm x)
+  ∑ y, (G.b x y) + (G.c x)
 
-variable [DecidableRel G.Adj]
+noncomputable
+instance : DecidableRel G.Adj := by exact Classical.decRel G.Adj
 
 noncomputable
 instance : Fintype ↑(G.neighborSet x) := by exact Fintype.ofFinite ↑(G.neighborSet x)
 
-set_option linter.unusedDecidableInType false
+--set_option linter.unusedDecidableInType false
 -- Example 0.4 (Combinatorial Degree)
 lemma degreeWithStandardWeights (h : StandardWeights G) (x : X) :
     G.deg x = G.degree x := by
@@ -54,14 +59,14 @@ lemma degreeWithStandardWeights (h : StandardWeights G) (x : X) :
   rw [h.killingTerm_zero, add_zero]
   rw [Finset.cast_card]
   rw [← Finset.sum_filter_ne_zero]
-  have (y : X) : G.edgeWeight x y ≠ 0 → G.edgeWeight x y = 1 := by
-    have := h.edgeWeight_binary' x y
+  have (y : X) : G.b x y ≠ 0 → G.b x y = 1 := by
+    have := h.edgeWeight_binary x y
     intro h
     cases this with
     | inl h1 => simp_all
     | inr h2 => exact h2
-  have : ∑ x_1 with G.edgeWeight x x_1 ≠ 0, G.edgeWeight x x_1 =
-      ∑ x_1 with G.edgeWeight x x_1 ≠ 0, 1 := by
+  have : ∑ x_1 with G.b x x_1 ≠ 0, G.b x x_1 =
+      ∑ x_1 with G.b x x_1 ≠ 0, 1 := by
     congr!
     simp_all
   rw [this]
@@ -70,12 +75,12 @@ lemma degreeWithStandardWeights (h : StandardWeights G) (x : X) :
   constructor
   · intro h
     simp only [ne_eq, Finset.mem_filter, Finset.mem_univ, true_and] at h
-    have : 0 < G.edgeWeight x a := by exact pos_of_ne_zero h
+    have : 0 < G.b x a := by exact pos_of_ne_zero h
     simp only [Set.toFinset_setOf, Finset.mem_filter, Finset.mem_univ, true_and]
     exact (G.edgeDef x a).mpr this
   simp only [Set.toFinset_setOf, Finset.mem_filter, Finset.mem_univ, true_and, ne_eq]
   intro h
-  suffices G.edgeWeight x a > 0 by exact pos_iff_ne_zero.mp this
+  suffices G.b x a > 0 by exact pos_iff_ne_zero.mp this
   exact (G.edgeDef x a).mp h
 
 lemma degreeWithStandardWeightsCard (h : StandardWeights G) (x : X) :
---
+++ GraphsAndDiscreteDirichletSpaces/Section012'.lean
@@ -0,0 +1,157 @@
+import GraphsAndDiscreteDirichletSpaces.Section011'
+
+open NNReal
+
+set_option linter.unusedFintypeInType false
+namespace GraphOver
+
+variable {X : Type*} (G : GraphOver X) (x : X) [Fintype X]
+
+-- Section 0.1.2
+--variable [TopologicalSpace X] [DiscreteTopology X]
+--variable {F : Type*} [ContinuousMapClass F X ℝ] (f : F)
+-- Don't deal with topologies, just consider all functions
+
+noncomputable
+instance : DecidableEq X := Classical.typeDecidableEq X
+
+-- Operators
+/-A natural basis for C (X) consists of characteristic functions 1x
+which take the value 1 at x and are 0 otherwise.-/
+noncomputable
+def stdBasis := (Pi.basisFun (η := X) (R := ℝ))
+
+scoped notation "𝟙_" y:max => Pi.single y 1
+
+noncomputable
+def matrixAssociatedToOp (L : (X → ℝ) →ₗ[ℝ] (X → ℝ)) :=
+  LinearMap.toMatrix stdBasis stdBasis L
+
+-- Matrix associated to L
+noncomputable
+abbrev l (L : (X → ℝ) →ₗ[ℝ] (X → ℝ)) := matrixAssociatedToOp L
+
+noncomputable
+def opInducedByMatrix (M : Matrix X X ℝ) :=
+  (LinearMap.toMatrix stdBasis stdBasis).symm M
+
+-- Operator induced by Matrix M (or what should really be l)
+noncomputable
+abbrev L (M : Matrix X X ℝ) := opInducedByMatrix M
+
+-- A direct calculation gives that l (x, y) = L1y (x)
+lemma opMatrixEntryVal (L : (X → ℝ) →ₗ[ℝ] (X → ℝ)) (x y : X) :
+    (l L) x y = L (𝟙_y) x := by
+  simp [matrixAssociatedToOp, stdBasis, LinearMap.toMatrix_eq_toMatrix', LinearMap.toMatrix'_apply]
+
+/-
+Clearly, to any operator L, there exists a unique function l : X × X −→ R with
+L f (x) =  ∑︁  y ∈X  l(x, y) f (y)  for all f ∈ C (X) and x ∈ X.
+-/
+lemma opDef (L : (X → ℝ) →ₗ[ℝ] (X → ℝ)) (f : X → ℝ) :
+  (L f) x = ∑ y, (l L) x y * f y := by
+  unfold l
+  have : f = stdBasis.equivFun.symm f := by rfl
+  nth_rw 1 [this, Module.Basis.equivFun_symm_apply stdBasis f]
+  simp only [map_sum, map_smul, Finset.sum_apply, Pi.smul_apply, smul_eq_mul]
+  congr
+  ext y
+  rw [mul_comm]
+  congr
+
+-- can I get this notion of symmetry out of Mathlib somehow?
+-- I don't want to have to define an inner product
+def IsSymmOp (L : (X → ℝ) →ₗ[ℝ] (X → ℝ)) := (l L).IsSymm
+
+-- remember to really prove every little statement
+
+-- Forms
+
+open LinearMap
+
+noncomputable
+def matrixAssociatedToForm (Q : LinearMap.BilinForm ℝ (X → ℝ)) :=
+  BilinForm.toMatrix' (n := X) (R₁ := ℝ) Q
+
+noncomputable
+def formInducedByMatrix (M : Matrix X X ℝ) : (X → ℝ) →ₗ[ℝ] (X → ℝ) →ₗ[ℝ] ℝ :=
+  (Matrix.toBilin' M)
+
+example (Q : LinearMap.BilinForm ℝ (X → ℝ)) (x y : X) :
+    Q (𝟙_x) (𝟙_y) = (matrixAssociatedToForm Q) x y := by rfl
+
+example (Q : LinearMap.BilinForm ℝ (X → ℝ)) (x : X) :
+    Q (𝟙_x) 1 = ∑ z, (matrixAssociatedToForm Q) x z := by
+  unfold matrixAssociatedToForm
+  simp only [BilinForm.toMatrix'_apply, ← map_sum, Finset.univ_sum_single]
+  rfl
+
+-- a form is symmetric iff associated matrix is symmetric
+#check LinearMap.BilinForm.isSymm_iff_basis stdBasis
+#check (LinearMap.BilinForm.isSymm_iff_basis stdBasis).mpr
+#check LinearMap.BilinForm.isSymm_iff
+#check LinearMap.BilinForm.isSymm_def
+#check stdBasis (X := X)
+
+example (l : Matrix X X ℝ) (hypl : Matrix.IsSymm l) (f g : X → ℝ) :
+    (formInducedByMatrix l) f g = ∑ x, ∑ y, l x y * f x * g y := by
+  --unfold formInducedByMatrix
+  -- show that the form is symmetric if matrix is symmetric
+  have : BilinForm.IsSymm (formInducedByMatrix l) := by
+    unfold formInducedByMatrix
+    sorry
+  have := BilinForm.isSymm_def.mp this
+  have := Matrix.IsSymm.ext_iff.mp hypl
+  -- do this on paper
+  sorry
+
+-- put 4-part eqn from p. 9
+
+noncomputable
+def associatedFormFun (f g : X → ℝ) :=
+  (1/2) * ∑ x, ∑ y, (G.b x y) * (f x - f y) * (g x - g y) +
+    ∑ x, (G.c x) * f x * g x
+
+open Finset
+
+instance associatedFormBilinearMap : IsBilinearMap (R := ℝ) G.associatedFormFun where
+  add_left f g h := by
+    unfold associatedFormFun
+    apply sub_eq_zero.mp
+    simp [sub_add_eq_sub_sub]
+    ring_nf
+    simp [sum_add_distrib]
+    ring
+  smul_left c f g := by
+    unfold associatedFormFun
+    field_simp
+    ring_nf
+    simp [sum_add_distrib]
+    ring_nf
+    simp [mul_sum]
+    ring_nf
+  add_right f g h := by
+    unfold associatedFormFun
+    apply sub_eq_zero.mp
+    simp [sub_add_eq_sub_sub]
+    ring_nf
+    simp [sum_add_distrib]
+    ring
+  smul_right c f g := by
+    unfold associatedFormFun
+    ring_nf
+    simp [sum_add_distrib]
+    ring_nf
+    simp [mul_sum]
+    ring_nf
+    apply sub_eq_zero.mp
+    ring_nf
+
+noncomputable
+def associatedForm := G.associatedFormBilinearMap.toLinearMap
+
+lemma associatedForm_apply {f g} : G.associatedForm f g = G.associatedFormFun f g := by rfl
+
+-- resume at p. 10
+
+end GraphOver
