ec3f874f553ebd6b67fb1b81c5cbffc6f4f6f3b6
Gregory Wickham <gwickham99@gmail.com>
2026-06-08 22:15:04 -0400

    a little more cleanup

--- GraphsAndDiscreteDirichletSpaces/Section012'.lean
+++ GraphsAndDiscreteDirichletSpaces/Section012'.lean
@@ -132,20 +132,16 @@ In particular, Q is symmetric if and only if the associated matrix l is symmetri
 
 example (Q : LinearMap.BilinForm ℝ (X → ℝ)) : Q.IsSymm ↔ (l_Bilin Q).IsSymm := by
   rw [LinearMap.BilinForm.isSymm_iff, LinearMap.isSymm_def, Matrix.IsSymm.ext_iff]
-  unfold l_Bilin
-  unfold matrixAssociatedToForm
-  simp only [Real.ringHom_apply, BilinForm.toMatrix'_apply]
+  simp only [l_Bilin, matrixAssociatedToForm, Real.ringHom_apply, BilinForm.toMatrix'_apply]
   constructor
-  · intro h i j
-    exact Real.ext_cauchy (congrArg Real.cauchy (h (𝟙_j) (𝟙_i)))
+  · intro h i j; grind
   intro h f g
   -- decompose f and g into basis vectors, then use linearity
   have : f = stdBasis.equivFun.symm f := by rfl
   rw [this, Module.Basis.equivFun_symm_apply stdBasis f]
   have : g = stdBasis.equivFun.symm g := by rfl
   rw [this, Module.Basis.equivFun_symm_apply stdBasis g]
-  unfold stdBasis
-  simp only [Pi.basisFun_apply, map_sum, map_smul, LinearMap.coe_sum, coe_smul, Finset.sum_apply,
+  simp only [stdBasis, Pi.basisFun_apply, map_sum, map_smul, LinearMap.coe_sum, coe_smul, Finset.sum_apply,
     Pi.smul_apply, smul_eq_mul, Finset.mul_sum]
   rw [Finset.sum_comm]
   ring_nf
@@ -168,25 +164,20 @@ open scoped Matrix
 example (h : l.IsSymm) :
     ∑ (x : X), ∑ (y : X), (l x y) * f x * g y
     = ∑ (y : X), ((L_Mat l) f) y * g y := by
-  unfold L_Mat
-  unfold opInducedByMatrix
-  simp only [toMatrix_symm]
-  unfold stdBasis
-  simp only [Matrix.toLin_apply, Pi.basisFun_apply, Finset.sum_apply, Pi.smul_apply, smul_eq_mul]
+  simp only [L_Mat, opInducedByMatrix, toMatrix_symm, stdBasis, Matrix.toLin_apply,
+    Pi.basisFun_apply, Finset.sum_apply, Pi.smul_apply, smul_eq_mul]
   have : f = stdBasis.equivFun.symm f := by rfl
   rw [this, Module.Basis.equivFun_symm_apply stdBasis f]
-  simp only [stdBasis, Pi.basisFun_apply, Finset.sum_apply, Pi.smul_apply, smul_eq_mul, map_sum,
-    map_smul, Finsupp.coe_finset_sum, Finsupp.coe_smul]
   have (x : X) : (((Pi.basisFun ℝ X).repr (𝟙_x)) : X → ℝ) = 𝟙_x := by
-    simp_all only [Module.Basis.equivFun_symm_apply]
-    rfl
-  simp only [this]
+    simp_all only [Module.Basis.equivFun_symm_apply]; rfl
+  simp only [stdBasis, Pi.basisFun_apply, Finset.sum_apply, Pi.smul_apply, smul_eq_mul, map_sum,
+    map_smul, Finsupp.coe_finset_sum, Finsupp.coe_smul, this]
   rw [Finset.sum_comm]
   congr
   ext y
   simp only [Pi.single_apply, mul_ite, mul_one, mul_zero, Finset.sum_ite_eq, Finset.mem_univ,
     ↓reduceIte, Matrix.mulVec_eq_sum, Finset.sum_apply, Pi.smul_apply, smul_eq_mul, op_smul_eq_smul,
-    Matrix.transpose_apply]
+    Matrix.transpose_apply] -- the next line cannot be merge into this for some reaon
   simp only [mul_comm]
   rw [← Finset.mul_sum]
   congr
@@ -291,15 +282,6 @@ lemma associatedForm_apply {f g} : G.associatedForm f g = G.associatedFormFun f 
 noncomputable
 abbrev Q_bc := G.associatedForm
 
-#check G.Q_bc
-
--- resume at p. 10
---@[simp] -- re-work proofs to use singleton membership instead of equality
-/-lemma sum_with_cond_eq {M : Type*} [AddCommMonoid M] (X : Type*) [Fintype X] (a : X) (f : X → M) :
-    ∑ (x : X) with x = a, f x = f a := calc
-  ∑ x with x = a, f x = ∑ x ∈ {a}, f x := by congr; grind [Set.set_compr_eq_eq_singleton]
-  _ = f a := sum_singleton (f ·) a-/
-
 @[simp]
 lemma no_loop (x : X) : G.b x x = 0 := by linarith [not_imp_not.mpr (G.edgeDef x x).mpr (G.irrefl (v := x))]
 
@@ -335,9 +317,7 @@ example : G.Q_bc (𝟙_x) (𝟙_x) = G.deg x := by
     rw [Finset.sum_eq_sum_diff_singleton_add (i := x) (by simp)]
     congr
     · ext y
-      by_cases h : y = x
-      · simp_all
-      simp_all
+      by_cases h : y = x <;> simp_all
     simp_all
   simp only [Pi.single_eq_same, this, two_mul]
   congr 1
@@ -357,15 +337,14 @@ example : G.Q_bc (𝟙_x) (𝟙_x) = G.deg x := by
 example (x y : X) (h : x ≠ y) : G.Q_bc (𝟙_x) (𝟙_y) = - G.b x y := by
   simp only [associatedForm_apply, associatedFormFun, one_div]
   field_simp
-  rw [neq_basis_vecs_imp_sum_weighted_killingTerm_eq_zero (h := h), mul_zero, add_zero]
-  nth_rw 1 [Finset.sum_eq_sum_diff_singleton_add (i := x) (by simp)]
+  rw [neq_basis_vecs_imp_sum_weighted_killingTerm_eq_zero (h := h), mul_zero, add_zero,
+    Finset.sum_eq_sum_diff_singleton_add (i := x) (by simp)]
   nth_rw 2 [Finset.sum_eq_sum_diff_singleton_add (i := y) (by simp)]
   have : ↑(G.b x y) * ((𝟙_x : X → ℝ) x - (𝟙_x : X → ℝ) y) * ((𝟙_y : X → ℝ) x - (𝟙_y : X → ℝ) y)
     = - ↑(G.b x y) := by grind
   rw [this]
   have : ∑ x_1 ∈ univ \ {y}, ↑(G.b x x_1) * ((𝟙_x : X → ℝ) x - (𝟙_x : X → ℝ) x_1) * ((𝟙_y : X → ℝ) x - (𝟙_y : X → ℝ) x_1) = 0 := by
-    suffices ∑ x_1 ∈ univ \ {y}, ↑(G.b x x_1) * ((𝟙_x : X → ℝ) x - (𝟙_x : X → ℝ) x_1) * ((𝟙_y : X → ℝ) x - (𝟙_y : X → ℝ) x_1) = ∑ x_1 ∈ univ \ {y}, 0 by
-      simp_all only [Finset.sum_const_zero]
+    rw [← Finset.sum_const_zero]
     congr! with z h2
     grind
   rw [this, zero_add]
@@ -403,29 +382,18 @@ example (x y : X) (h : x ≠ y) : G.Q_bc (𝟙_x) (𝟙_y) = - G.b x y := by
   ring
 
 -- Furthermore
-example : G.Q_bc (𝟙_x) 1 = G.c x := by
-  unfold Q_bc
-  simp only [associatedForm_apply, associatedFormFun, one_div, Pi.one_apply, sub_self, mul_zero,
-    sum_const_zero, mul_one, zero_add]
-  rw [Finset.sum_eq_sum_diff_singleton_add (i := x) (h := by grind)]
-  simp only [Pi.single_eq_same, mul_one]
+lemma associatedForm_at_basisVec_eq_killingTerm : G.Q_bc (𝟙_x) 1 = G.c x := by
   have : ∑ x_1 ∈ univ \ {x}, ↑(G.c x_1) * (𝟙_x : X → ℝ) x_1 = 0 := by
-    suffices ∑ x_1 ∈ univ \ {x}, ↑(G.c x_1) * (𝟙_x : X → ℝ) x_1
-           = ∑ x_1 ∈ (univ \ {x}), 0 by
-      simp only [Finset.sum_const_zero] at this
-      exact this
-    congr! with z h2
-    grind
-  simp [this]
+    rw [← Finset.sum_const_zero]; congr! with z h2; grind
+  simp
+  grind [Finset.sum_eq_sum_diff_singleton_add]
 
 #check G.Q_bc
 -- Clearly
-example : G.Q_bc.IsSymm := by
+lemma associatedForm_isSymm : G.Q_bc.IsSymm := by
   simp only [isSymm_def, associatedForm_apply, associatedFormFun, one_div, Real.ringHom_apply]
   intro f g
-  congr 1; swap
-  · grind
-  grind
+  congr 1 <;> grind
 
 -- more conditions will be added later to get regular dirichlet forms
 
@@ -440,18 +408,18 @@ structure DirichletProp (Q : (X → ℝ) →ₗ[ℝ] (X → ℝ) →ₗ[ℝ] ℝ
 --maybe define this using derivatives or some sobolev/function properties
 -- is this like regularity or something?
 -- could find a way to express this with Lipschitz continuity, but it's probably not worth it
-
+omit [Fintype X] in
 theorem DirichletProp_def (Q : (X → ℝ) →ₗ[ℝ] (X → ℝ) →ₗ[ℝ] ℝ) : DirichletProp Q ↔
-  (∀ (f g : X → ℝ), (∀ (x y : X), |f x - f y| ≤ |g x - g y|) → (∀ (x : X), (|f x| ≤ |g x|)) → Q f f ≤ Q g g) := by
+  (∀ (f g : X → ℝ), (∀ (x y : X), |f x - f y| ≤ |g x - g y|) → (∀ (x : X), (|f x| ≤ |g x|)) →
+    Q f f ≤ Q g g) := by
   constructor
   · exact fun a f g a_1 a_2 ↦ a.eq f g a_1 a_2
   exact fun a ↦ { eq := a }
 
-example : DirichletProp G.Q_bc := by
+lemma associatedForm_is_Dirichlet : DirichletProp G.Q_bc := by
   simp only [DirichletProp_def]
   intro f g h1 h2
-  unfold Q_bc
-  simp [associatedForm_apply, associatedFormFun]
+  simp [Q_bc, associatedForm_apply, associatedFormFun]
   field_simp
   gcongr 1
   · gcongr 3 with a ha b hb
@@ -466,33 +434,12 @@ example : DirichletProp G.Q_bc := by
   simp [h2] -- this is where I use the questionable norm thing
 
 structure DirichletForm (Q : (X → ℝ) →ₗ[ℝ] (X → ℝ) →ₗ[ℝ] ℝ) : Prop where
-  protected dirichletProp : DirichletProp Q
-  protected symm : Q.IsSymm
+  dirichletProp : DirichletProp Q
+  symm : Q.IsSymm
 
-instance : DirichletForm G.Q_bc where -- the proofs below are copied from example above
-  dirichletProp := by
-    simp only [DirichletProp_def]
-    intro f g h1 h2
-    unfold Q_bc
-    simp [associatedForm_apply, associatedFormFun]
-    field_simp
-    gcongr 1
-    · gcongr 3 with a ha b hb
-      rw [← sq_abs]
-      nth_rw 2 [← sq_abs]
-      gcongr 1
-      simp [h1]
-    gcongr 3 with x hx
-    rw [← sq_abs]
-    nth_rw 2 [← sq_abs]
-    gcongr 1
-    simp [h2]
-  symm := by
-    simp only [isSymm_def, associatedForm_apply, associatedFormFun, one_div, Real.ringHom_apply]
-    intro f g
-    congr 1; swap
-    · grind
-    grind
+instance : DirichletForm G.Q_bc where
+  dirichletProp := G.associatedForm_is_Dirichlet
+  symm := G.associatedForm_isSymm
 
 -- Definition 0.6 (Laplacian)
 def Laplacian (f : X → ℝ) : X → ℝ :=
