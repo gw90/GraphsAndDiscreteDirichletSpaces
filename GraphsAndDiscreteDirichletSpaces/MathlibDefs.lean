3889748d6b13eeb8d39c6a86a14c46b0dbe0559b
Gregory Wickham <gwickham99@gmail.com>
2026-06-10 17:49:30 -0400

    more Mathlib-ification

--- GraphsAndDiscreteDirichletSpaces/MathlibDefs.lean
+++ GraphsAndDiscreteDirichletSpaces/MathlibDefs.lean
@@ -3,26 +3,35 @@ import Mathlib
 open NNReal
 
 @[ext]
-structure BasicWeightedGraph (X : Type*) extends SimpleGraph X where
+structure WeightedGraph (X : Type*) extends SimpleGraph X where
   edgeWeight : X → X → ℝ≥0
   edgeWeight_symm : IsSymmOp edgeWeight
   edgeDef (u v : X) : Adj u v ↔ 0 < edgeWeight u v -- make a lemma for the contrapositive of this
 
+variable {X : Type*}
+
+@[simp]
+lemma WeightedGraph.no_loop {x : X} (G : WeightedGraph X) : G.edgeWeight x x = 0 := by
+  linarith [not_imp_not.mpr (G.edgeDef x x).mpr (G.irrefl (v := x))]
+
+lemma WeightedGraph.edgeWeight_symm_apply (G : WeightedGraph X) (y z : X) :
+    G.edgeWeight y z = G.edgeWeight z y := G.edgeWeight_symm.symm_op y z
+
 @[ext]
-structure WeightedGraph (X : Type*) extends BasicWeightedGraph X where
+structure WeightedGraphWithKillingTerm (X : Type*) extends WeightedGraph X where
   killingTerm : X → ℝ≥0
 
 -- Define predicates to recover simple graphs and produce them
 -- Do everything for WeightedGraph, then include special cases
 
-variable {X : Type*} (G : WeightedGraph X)
+variable (G : WeightedGraphWithKillingTerm X)
 
 -- Maybe let this be a special corollary that uniquely includes decidability hypotheses
 -- #check fun x y ↦ if G.Adj x y then 1 else 0
 
 --instance {x y : X} : Decidable (G.Adj x y) := by infer_instance
 
-structure StandardWeights (G : WeightedGraph X) : Prop where
+structure StandardWeights (G : WeightedGraphWithKillingTerm X) : Prop where
   edgeWeight_Adj_iff : G.Adj x y ↔ G.edgeWeight x y = 1
   edgeWeight_NotAdj_iff : ¬ G.Adj x y ↔ G.edgeWeight x y = 0
   killingTerm_zero : G.killingTerm = 0
@@ -47,7 +56,7 @@ lemma standardWeightEdgeSet (h : StandardWeights G) :
 
 variable [Fintype X]
 
-def WeightedGraph.degree (x : X) : ℝ≥0 :=
+def WeightedGraphWithKillingTerm.degree (x : X) : ℝ≥0 :=
   ∑ y, (G.edgeWeight x y) + (G.killingTerm x)
 
 noncomputable
@@ -55,10 +64,59 @@ instance : Fintype ↑(G.neighborSet x) := Fintype.ofFinite (G.neighborSet x)
 
 lemma degreeWithStandardWeights (h : StandardWeights G) (x : X) :
     G.degree x = G.toSimpleGraph.degree x := by
-  simp only [WeightedGraph.degree, h.killingTerm_zero, Pi.zero_apply, add_zero,
+  simp only [WeightedGraphWithKillingTerm.degree, h.killingTerm_zero, Pi.zero_apply, add_zero,
     ← G.card_neighborFinset_eq_degree, G.neighborFinset_def, SimpleGraph.neighborSet,
     Finset.cast_card]
   rw [← Finset.sum_filter_ne_zero]
   congr! with y hy
   · grind [standardWeights_edgeWeight_neq_zero_iff_eq_one]
   exact h.edgeWeight_Adj_iff.mp ((G.toSimpleGraph.mem_neighborFinset x y).mp hy)
+
+
+-- Definiton 0.5
+@[simp]
+noncomputable
+def WeightedGraphWithKillingTerm.associatedFormFun (f g : X → ℝ) :=
+  (1/2) * ∑ x, ∑ y, (G.edgeWeight x y) * (f x - f y) * (g x - g y) +
+    ∑ x, (G.killingTerm x) * f x * g x
+
+open Finset
+
+instance WeightedGraphWithKillingTerm.associatedFormBilinearMap :
+    IsBilinearMap (R := ℝ) G.associatedFormFun where
+  add_left f g h := by
+    apply sub_eq_zero.mp
+    simp [sub_add_eq_sub_sub]
+    ring_nf
+    simp [sum_add_distrib]
+    ring
+  smul_left c f g := by
+    unfold WeightedGraphWithKillingTerm.associatedFormFun
+    field_simp
+    ring_nf
+    simp [sum_add_distrib]
+    ring_nf
+    simp [mul_sum]
+    ring_nf
+  add_right f g h := by
+    apply sub_eq_zero.mp
+    simp [sub_add_eq_sub_sub]
+    ring_nf
+    simp [sum_add_distrib]
+    ring
+  smul_right c f g := by
+    unfold WeightedGraphWithKillingTerm.associatedFormFun
+    ring_nf
+    simp [sum_add_distrib]
+    ring_nf
+    simp [mul_sum]
+    ring_nf
+    apply sub_eq_zero.mp
+    ring_nf
+
+noncomputable
+def WeightedGraphWithKillingTerm.associatedForm := G.associatedFormBilinearMap.toLinearMap
+
+lemma WeightedGraphWithKillingTerm.associatedForm_apply {f g} : G.associatedForm f g =
+  (1/2) * ∑ x, ∑ y, (G.edgeWeight x y) * (f x - f y) * (g x - g y) +
+    ∑ x, (G.killingTerm x) * f x * g x := by rfl
